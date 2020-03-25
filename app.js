2ab42b58b0ee1a1f966ac02fb58dc59430b1e7de
Celia <celiafrom12@gmail.com>
2020-03-25 15:24:17 +0000

    js, object with flightpath

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 
 const flightpath = {
-  curviness: 1;  //curviness the amount of curve the image is gonna take
+  curviness: 1.25, //curviness the amount of curve the image is gonna take
+  autoRotate:true,
+  values: [
+    {x:100, y: -20}
+  ]
 }
